d30fbadbd26598f70ab8219f0d21f8530e53a5e8
Zach Romani <zach.romani@hotelengine.com>
2023-03-14 07:15:04 -0600

    Updated packer compile

--- dot-config/nvim/plugin/packer_compiled.lua
+++ dot-config/nvim/plugin/packer_compiled.lua
@@ -106,7 +106,7 @@ _G.packer_plugins = {
   },
   ["copilot.lua"] = {
     commands = { "Copilot" },
-    config = { "\27LJ\2\n„\4\0\0\5\0\16\0\0216\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\b\0005\3\3\0005\4\4\0=\4\5\0035\4\6\0=\4\a\3=\3\t\0025\3\n\0005\4\v\0=\4\5\3=\3\f\0025\3\r\0=\3\14\0024\3\0\0=\3\15\2B\0\2\1K\0\1\0\26server_opts_overrides\14filetypes\1\0\t\thelp\1\6.\1\rhgcommit\1\14gitcommit\1\14gitrebase\1\tyaml\1\bsvn\1\bcvs\1\rmarkdown\1\15suggestion\1\0\6\tprev\n<M-[>\vaccept\n<M-l>\fdismiss\n<C-]>\16accept_word\1\16accept_line\1\tnext\n<M-]>\1\0\3\17auto_trigger\2\fenabled\2\rdebounce\3K\npanel\1\0\1\25copilot_node_command\tnode\vlayout\1\0\2\nratio\4š³æÌ\t™³æþ\3\rposition\vbottom\vkeymap\1\0\5\frefresh\agr\14jump_next\a]]\topen\v<M-CR>\vaccept\t<CR>\14jump_prev\a[[\1\0\2\fenabled\2\17auto_refresh\1\nsetup\fcopilot\frequire\0" },
+    config = { "\27LJ\2\n„\4\0\0\5\0\16\0\0216\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\b\0005\3\3\0005\4\4\0=\4\5\0035\4\6\0=\4\a\3=\3\t\0025\3\n\0005\4\v\0=\4\5\3=\3\f\0025\3\r\0=\3\14\0024\3\0\0=\3\15\2B\0\2\1K\0\1\0\26server_opts_overrides\14filetypes\1\0\t\tyaml\1\14gitrebase\1\bsvn\1\rhgcommit\1\thelp\1\bcvs\1\rmarkdown\1\14gitcommit\1\6.\1\15suggestion\1\0\6\fdismiss\n<C-]>\vaccept\n<M-l>\tnext\n<M-]>\tprev\n<M-[>\16accept_word\1\16accept_line\1\1\0\3\17auto_trigger\2\rdebounce\3K\fenabled\2\npanel\1\0\1\25copilot_node_command\tnode\vlayout\1\0\2\nratio\4š³æÌ\t™³æþ\3\rposition\vbottom\vkeymap\1\0\5\14jump_prev\a[[\14jump_next\a]]\topen\v<M-CR>\vaccept\t<CR>\frefresh\agr\1\0\2\fenabled\2\17auto_refresh\1\nsetup\fcopilot\frequire\0" },
     loaded = false,
     needs_bufread = false,
     only_cond = false,
