fde19f83da76f870cb9b07747b33b096a49189a8
Zach Romani <zach.romani@hotelengine.com>
2024-03-03 09:10:01 -0700

    Udpated c-p to use find files
    Not sure why I was using "git file" command.

--- dot-config/nvim/after/plugin/telescope.lua
+++ dot-config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader><leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader><leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader><leader>g', builtin.lsp_references, {})
 vim.keymap.set('n', '<leader><leader>s', function()
