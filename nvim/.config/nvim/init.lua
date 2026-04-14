667b1e505c8b2e5bdeddd2044629d91170cf15c0
Zach Romani <zach.romani@hotelengine.com>
2026-04-14 13:23:14 -0600

    Fix lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,6 @@
+-- packer.nvim uses vim.tbl_islist which is deprecated in Neovim 0.10+
+-- Silently redirect to the replacement before packer loads
+vim.tbl_islist = vim.islist
+
 require("romani")
 
